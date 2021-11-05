879c1524d9861a95720f811d2a388ec3edeb1b32
anushkasahoo152 <93137129+anushkasahoo152@users.noreply.github.com>
2021-11-05 15:42:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 580);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose' , gotPoses);
+}
+function draw() {
+    background('#87CEEB');
+    document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
+    textSize(difference);
+    fill('white');
+    text('Welcome', 50, 300);
+}
+function modelLoaded() {
+    console.log("PoseNet is Initialized");
+}
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX ="+ leftWristX +"rightWristX =" + rightWristX +"difference" + difference);
+    }
+}
